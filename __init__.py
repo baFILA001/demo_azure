7116284f6b5b4608e27ad32fc3929c2a8b27831c
Vikash Bafila <vikashbafila321@gmail.com>
2024-10-10 12:14:29 +0530

    hello

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 import logging
 import azure.functions as func
 
+#hello
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
